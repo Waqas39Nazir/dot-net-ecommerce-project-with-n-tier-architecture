e905f699a2dabee565be9d514c9e3e4ac22a0f00
Waqas Nazir <waqas3399nazir@gmail.com>
2026-01-30 16:28:13 +0500

    Fixes

--- DNMVCCP.DataAccess/Repository/CategoryRepository.cs
+++ DNMVCCP.DataAccess/Repository/CategoryRepository.cs
@@ -20,10 +20,6 @@ namespace DNMVCCP.DataAccess.Repository
             _db = db;
         }
 
-        public void Save()
-        {
-            _db.SaveChanges();
-        }
 
         public void Update(Category obj)
         {
